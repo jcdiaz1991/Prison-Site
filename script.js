f6d5be97de3e9de3ba7dae965b87855396ce505e
Juan Diaz <juandiaz9114@gmail.com>
2018-08-24 18:29:32 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", slideInLeftBig);
+
+function slideInLeftBig(){
+  var header = document.getElementById('header');
+  header.classList.add("animated","fadeInLeftBig","slower");
+}
